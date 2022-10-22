c129e60a4be49027bb96f5c9d9de845a27242f84
aseruneko <aseruneko@gmail.com>
2022-10-22 19:23:22 +0900

    プレイヤーJOINまで

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const fs = require('fs').promises;
-const crypto = require("crypto");
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
-const readData = require("./src/read-data.js");
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+const Room = require("./src/room");
+var rooms = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
@@ -16,48 +19,57 @@ app.get("/", (req, res) => {
   res.render("./index.ejs");
 });
 
-app.get("/book-rooms/new", (req, res) => {
-  res.render("./book-rooms/new.ejs");
+app.get("/rooms/new", (req, res) => {
+  res.render("./rooms/new/index.ejs");
 });
 
-app.get("/book-rooms/:roomId", (req,res) => {
-  fs.readFile("public/data.json", 'utf-8').then((file) => {
-    const data = JSON.parse(file).data;
-    console.log(data);
-    console.log(req.params.bookRoomId);
-    const bookRoom = data.find(datum => datum.roomId === req.params.roomId);
-    console.log(bookRoom);
-    res.render("./book-rooms/book-room.ejs", bookRoom);
-  })
+app.get("/rooms/join", (req, res) => {
+  res.render("./rooms/join/index.ejs");
 });
 
-app.post('/api/add/',(req, res) =>  {
-  const firstValue = parseInt(req.body.firstValue);
-  const secondValue = parseInt(req.body.secondValue);
-  const sum = firstValue + secondValue;
-  res.json({sum: sum, firstValue: firstValue, secondValue: secondValue});
+app.get("/rooms/:id", (req,res) => {
+  const room = rooms.find(room => room.id == req.params.id);
+  res.render("./rooms/room.ejs", room);
 });
 
-app.post('/api/book-rooms/create',(req, res) =>  {
-  const bookRoomId = crypto.randomUUID();
-  const data = {
-    roomId: bookRoomId,
-    roomTitle: req.body.roomTitle,
-    hostName: req.body.playerName,
-    minuteMax: parseInt(req.body.minuteMax),
-    pageNum: parseInt(req.body.pageNum),
-    status: "WAITING",
-    players: [{
-      name: req.body.playerName
-    }]
+app.post('/api/rooms/create',(req, res) =>  {
+  const room = Room.create(
+    req.body.roomName,
+    parseInt(req.body.minuteMax),
+    parseInt(req.body.pageNum),
+    req.body.playerName
+  );
+  rooms.push(room);
+  res.json({roomId: room.id, userId: room.players[0].id});
+});
+
+app.post('/api/rooms/join', (req,res) => {
+  const roomId = req.body.roomId;
+  const playerName = req.body.playerName;
+  const room = rooms.find(room => room.id == roomId);
+  const player = room.addPlayer(playerName);
+  io.emit('roomStatusUpdation', {roomId: req.body.roomId});
+  res.json({roomId: roomId, userId: player.id});
+});
+
+app.post('/api/books/create',(req,res) => {
+  const userId = req.body.userId;
+  const roomId = req.body.roomId;
+  const title = req.body.title;
+  const room = rooms.find(room => room.id == roomId);
+  room.addBook(title);
+  if(room.allTitleSubmitted()) {
+    room.startWriting();
+    io.emit('roomStatusUpdation', {roomId: req.body.roomId});
   }
-  fs.readFile("public/data.json", 'utf-8').then((file) => {
-    const readData = JSON.parse(file);
-    readData.data.push(data);
-    fs.writeFile("public/data.json", JSON.stringify(readData)).then(_ => {
-      res.json({bookRoomId: bookRoomId});
-    })
-  })
+  res.json();
+})
+
+app.post('/api/rooms/start',(req, res) => {
+  const room = rooms.find(room => room.id == req.body.roomId);
+  room.startTitling();
+  io.emit('roomStatusUpdation', {roomId: req.body.roomId});
+  res.json();
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+http.listen(port, () => console.log(`Example app listening on port ${port}!`));
